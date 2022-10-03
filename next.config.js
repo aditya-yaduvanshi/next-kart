3724872796673cdd562f66193c2fc459f51b5204
Aditya Yaduvanshi <adityayaduvanshi0001@gmail.com>
2022-10-03 14:12:16 +0530

    tailwind & firebase setup

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+	reactStrictMode: true,
+	swcMinify: true,
+	env: {
+		apiKey: `${process.env.API_KEY}`,
+		authDomain: `${process.env.AUTH_DOMAIN}`,
+		projectId: `${process.env.PROJECT_ID}`,
+		storageBucket: `${process.env.STORAGE_BUCKET}`,
+		messagingSenderId: `${process.env.API_KEY}`,
+		appId: `${process.env.APP_ID}`,
+		measurementId: `${process.env.MEASUREMENT_ID}`,
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
